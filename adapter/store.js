c9b528b54c221148a55d5d8de4934d625e13b5bd
kanade <melty0404@gmail.com>
2021-04-11 14:01:15 +0900

    日時をy/m/d H:iのJSTに変換する

--- adapter/store.js
+++ adapter/store.js
@@ -6,10 +6,9 @@ const convertToJST = (strDate) => {
 
 const parseDate = (strDate) => {
   const date = convertToJST(strDate);
-  const [y, m, d] = date.toLocaleDateString('ja-JP').split('/');
   // eslint-disable-next-line no-unused-vars
   const [h, i, _] = date.toLocaleTimeString('ja-JP').split(':');
-  return `${y}年${m}月${d}日 ${h}時${i}分`;
+  return `${date.toLocaleDateString('ja-JP')} ${h}:${i}`;
 };
 
 exports.convertStore = (store) => ({
