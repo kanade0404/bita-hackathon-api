0162be9ba1a86b6c8dabd8dc5e1a25be54ce88f5
kanade <melty0404@gmail.com>
2021-04-11 12:59:58 +0900

    req.sessionを存在チェック

--- controllers/user.js
+++ controllers/user.js
@@ -96,7 +96,7 @@ exports.logout = (req, res) => {
   req.session.destroy((err) => {
     if (err) console.log('Error : Failed to destroy the session during logout.', err);
     req.user = null;
-    if (req.session.user) { req.session.user = null; }
+    if (req.session && req.session.user) { req.session.user = null; }
     res.cookie('connect.sid', '');
     res.cookie('userId', '');
     res.redirect(`${process.env.CLIENT_URL}/login`);
